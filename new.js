92408c25bc49ab765a9101e7e5ccafa4290a8585
FarrukhSajjad46 <farrukhsajjad46@gmail.com>
2024-01-10 15:26:12 +0500

    Initial commit

---
+++ new.js
@@ -0,0 +1,7 @@
+"use strict";
+let fname = "Farrukh Sajjad";
+let age = 22;
+let alive = true;
+console.log(fname);
+console.log(age);
+console.log(alive);
---
+++ new.ts
@@ -0,0 +1,6 @@
+let fname:String = "Farrukh Sajjad";
+let age:Number = 22;
+let alive:Boolean = true;
+console.log(fname)
+console.log(age)
+console.log(alive)
